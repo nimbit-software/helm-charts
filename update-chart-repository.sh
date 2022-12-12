2e322b81929d5a303dde83ec54d408b7a77ad773
Florian Breustedt <florian.breustedt@cpars.de>
2022-12-12 12:59:58 +0100

    charts

--- update-chart-repository.sh
+++ update-chart-repository.sh
@@ -7,7 +7,7 @@ do
     dir=${dir%*/}      # remove the trailing "/"
     echo "${dir##*/}"    # print everything after the final "/"
     
-    helm package $dir -d $dir
+    helm package $dir -d ./
 
 done
 
